822568781f793288296158dba5f215801242c37f
g-harel <gabrielj.harel@gmail.com>
2019-11-17 20:58:44 -0600

    color noteworthy http logs

--- logging/http_client.go
+++ logging/http_client.go
@@ -17,8 +17,23 @@ type WrappedRoundTripper struct {
 func (w *WrappedRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	start := time.Now()
 	res, err := w.original.RoundTrip(req)
+
+	timeMillisecond := time.Since(start).Truncate(time.Millisecond).Nanoseconds() / 1e6
+	colorCode := "" // none
+	if timeMillisecond > 1000 {
+		colorCode = "\u001b[33m" // yellow
+	}
+	if res.StatusCode != 200 {
+		colorCode = "\u001b[31m" // red
+	}
 	if err == nil {
-		fmt.Printf("%v %v %v\n", res.StatusCode, req.URL.String(), time.Since(start).Truncate(time.Millisecond))
+		fmt.Printf(
+			"%v%v %4vms %v\u001b[0m\n",
+			colorCode,
+			res.StatusCode,
+			timeMillisecond,
+			req.URL.String(),
+		)
 	}
 	return res, err
 }
